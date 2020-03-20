a00f49c968e70fe8e6d4840c22e1bb1527c6fef9
GGC-Github <kgc@kisec.com>
2020-03-20 18:51:28 +0900

    modified:AutoGit.sh newfile:test.txt

--- AutoGit.sh
+++ AutoGit.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-commit_des="$1"
-echo "$commit_des"
 echo "========== Git Auto Push Start =========="
 
-git status
-git add -A
-git status
-git commit -m "$commit_des"
-git push origin master
+add_file_chk=`git status | grep "nothing to commit"`
+if [ "$add_file_chk" != "" ]; then
+	echo "Not Found Add File"
+else
+	git status
+	git add -A
+	if [ "$1" != "" ]; then
+		commit_des="$1"
+	else
+		commit_des=`git status | egrep -v '^[A-Z]|^$|\(' | tr -d ' \t'`
+	fi
+	git status
+	git commit -m "$commit_des"
+	git push origin master
+fi
 
 echo "==========  Git Auto Push End  =========="
